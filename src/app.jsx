91194d9dd437a4cfcd11f831180ff3f8c25f8fdf
skipness <skipness123@gmail.com>
2019-06-26 15:54:01 +0800

    Add link to github repo

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,7 @@ import { Container, Grid } from '@material-ui/core';
 import LeftPanel from './component/left-panel/left-panel';
 import RightPanel from './component/right-panel/right-panel';
 import { CanvasContextProvider } from './component/canvas-context-provider';
+import GithubButton from './component/common/github-button';
 
 const App = () => (
   <Container className="app">
@@ -12,6 +13,7 @@ const App = () => (
         <RightPanel />
       </CanvasContextProvider>
     </Grid>
+    <GithubButton />
   </Container>
 );
 
