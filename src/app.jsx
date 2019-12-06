79e4b9588c2f5897c7573ff6d5b27a15792f7925
skipness <skipness123@gmail.com>
2019-12-06 16:00:21 +0800

    Add Google Analytics

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,17 @@
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import React from 'react';
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga';
 import CanvasContextProvider from './component/canvas-context-provider';
 import GithubButton from './component/common/github-button';
 import LeftPanel from './component/left-panel/left-panel';
 import RightPanel from './component/right-panel/right-panel';
 
 const App = () => {
+  useEffect(() => {
+    ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
+  }, []);
+
   return (
     <Container className='app'>
       <Grid container justify='center'>
